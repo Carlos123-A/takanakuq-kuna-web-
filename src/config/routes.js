21a52b07a88390cdfcd3e0b7d4b793cdb295654e
Carlos123-A <carlos.aguilar.v@tecsup.edu.pe>
2024-07-02 00:04:33 -0500

    proyecto terminado

---
+++ src/config/routes.js
@@ -0,0 +1,17 @@
+export const routes = [
+    { name: 'Inicio', path: '/' },
+    { name: 'Nosotros', path: '/us'},
+    { name: 'Contactanos', path: '/contact' },
+    { name: 'Políticas', path: '/about' }
+  ];
+
+export const images = [
+    {
+      src: '/public/Nosotros1.jpeg', 
+      alt: 'Descripción de la imagen 1'
+    },
+    {
+      src: '/public/Nosotros.jpeg', 
+      alt: 'Descripción de la imagen 2'
+    }
+  ];
